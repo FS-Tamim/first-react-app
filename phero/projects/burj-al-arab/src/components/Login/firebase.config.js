08ce4a1b2aac1f162740d622052b527f6da7b631
FS-Tamim <fstamim74@gmail.com>
2020-10-01 02:38:58 +0600

    coomit

---
+++ phero/projects/burj-al-arab/src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDz7IPi-e8yOnJBc0AnsqSz1EOAqa6DgT8",
+  authDomain: "burj-al-arab-d41e0.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-d41e0.firebaseio.com",
+  projectId: "burj-al-arab-d41e0",
+  storageBucket: "burj-al-arab-d41e0.appspot.com",
+  messagingSenderId: "857498931937",
+  appId: "1:857498931937:web:45390a895db62724e1dda0"
+};
+  export default firebaseConfig;
